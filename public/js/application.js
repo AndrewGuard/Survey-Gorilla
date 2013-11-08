ebc883593d007da408769f99f686fea690d6071d
Apprentice <apprentice@dbc-chicago031.local>
2013-11-08 16:39:47 -0600

    working on AJAX

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $("#add_question_button").on("click", function(event){
+    event.preventDefault();
+    $("#questions-box").append($("#first-question").html());
+  });
+
+  $("#remove_question_button").on("click", function(event){
+    event.preventDefault();
+    $(".question").last().remove();
+  });
+
+  // $("#create_survey_button").on("click", function(event){
+  //   event.preventDefault();
+
+
+  // });
+
+
 });

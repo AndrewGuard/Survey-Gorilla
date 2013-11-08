be4e749f19a059c5ef7bab9db52554fe4bc4753b
Apprentice <apprentice@dbc-chicago031.local>
2013-11-08 13:52:36 -0600

    Added seed file and route summary

--- db/migrate/20131108104522_create_created_surveys.rb
+++ db/migrate/20131108104522_create_created_surveys.rb
@@ -3,6 +3,7 @@ class CreateCreatedSurveys < ActiveRecord::Migration
     create_table :created_surveys do |t|
       t.integer :user_id
       t.string :title
+      t.string :image_file_name
 
       t.timestamps
     end

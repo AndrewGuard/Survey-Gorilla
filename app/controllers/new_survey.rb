aed070693e582087c6502f73b2cdd25b6a3a7c19
Apprentice <apprentice@dbc-chicago031.local>
2013-11-08 14:41:53 -0600

    Rearranged controller files to place wildcards last to not break /survey/new. Added new_survey.erb

--- app/controllers/new_survey.rb
+++
@@ -1,9 +0,0 @@
-# Create a new survey with its questions an associated responses
-get '/surveys/new' do
-  # NO AJAX/JS YET OR I WILL END YOU
-end
-
-# Save the survey with all its associated questions
-post '/surveys/new' do
-
-end

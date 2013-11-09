d93a7fe299d19317393f11194c242d7ee23b6506
Apprentice <apprentice@dbc-chicago031.local>
2013-11-09 12:27:25 -0600

    Fixed image pathing, resized images, added image to survey results page

--- lib/uploaders/photo_uploader.rb
+++ lib/uploaders/photo_uploader.rb
@@ -1,5 +1,8 @@
 class PhotoUploader < CarrierWave::Uploader::Base
-  storage :file
+  include CarrierWave::MiniMagick
+  process :resize_to_fill => [200, 200]
+
+   storage :file
 
   def store_dir
     'images'
